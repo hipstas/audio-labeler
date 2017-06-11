f1e59c4d53247882054855799761375f8a18f1dd
Stephen McLaughlin <stephen.mclaughlin@utexas.edu>
2017-06-11 18:14:50 -0500

    checkpoint

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
-from flask import Flask
-from flask_wtf import FlaskForm
-from wtforms import StringField
+# We need to import request to access the details of the POST request
+# and render_template, to render our templates (form and response)
+# we'll use url_for to get some URLs for the app on the templates
+from flask import Flask, render_template, request, url_for
+import unicodecsv
 
+# Initialize the Flask application
 app = Flask(__name__)
 
-class FlaskForm(Form):
-    text = StringField('Text')
+# Define a route for the default URL, which loads the form
+@app.route('/')
+def form():
+    return render_template('form_submit.html')
 
-
-@app.route("/")
+# Define a route for the action of the form, for example '/hello/'
+# We are also defining which type of requests this route is
+# accepting: POST requests in this case
+@app.route('/hello/', methods=['POST'])
 def hello():
-    form = FlaskForm(csrf_enabled=False)
-    return form
+    name=request.form['yourname']
+    email=request.form['youremail']
+    with open('output.txt','w') as fo:
+        fo.write(name+'\n')
+        fo.write(email+'\n')
+    return render_template('form_action.html', name=name, email=email)
 
-
-if __name__ == "__main__":
-app.run()
+# Run the app :)
+if __name__ == '__main__':
+  app.run(
+        host="0.0.0.0",
+        port=int("8000")
+  )
