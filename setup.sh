3511c0ce48f150c407a6f976576923eeff91ac2a
Stephen McLaughlin <stephen.mclaughlin@utexas.edu>
2017-06-13 15:15:40 -0500

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -3,13 +3,26 @@ wget -N https://raw.githubusercontent.com/stevemclaugh/audio-labeling-container/
 
 mkdir -p media
 mkdir -p templates
-mkdir -p static
+mkdir -p static/wavesurfer
 
 cd static
 wget -N https://raw.githubusercontent.com/stevemclaugh/audio-labeling-container/master/static/style.css
+
 cd /home/audio_labeler/templates
-wget  -N https://raw.githubusercontent.com/stevemclaugh/audio-labeling-container/master/templates/form_action.html
-wget  -N https://raw.githubusercontent.com/stevemclaugh/audio-labeling-container/master/templates/form_audio.html
-wget  -N https://raw.githubusercontent.com/stevemclaugh/audio-labeling-container/master/templates/form_video.html
+wget -N https://raw.githubusercontent.com/stevemclaugh/audio-labeling-container/master/templates/form_action.html
+wget -N https://raw.githubusercontent.com/stevemclaugh/audio-labeling-container/master/templates/form_audio.html
+wget -N https://raw.githubusercontent.com/stevemclaugh/audio-labeling-container/master/templates/form_video.html
+
+cd /home/audio_labeler/static/wavesurfer
+wget -N https://raw.githubusercontent.com/stevemclaugh/audio-labeling-container/master/static/wavesurfer/app.css
+wget -N https://raw.githubusercontent.com/stevemclaugh/audio-labeling-container/master/static/wavesurfer/app.js
+wget -N https://raw.githubusercontent.com/stevemclaugh/audio-labeling-container/master/static/wavesurfer/ribbon.css
+wget -N https://raw.githubusercontent.com/stevemclaugh/audio-labeling-container/master/static/wavesurfer/style.css
+wget -N https://raw.githubusercontent.com/stevemclaugh/audio-labeling-container/master/static/wavesurfer/wavesurfer.min.js
+wget -N https://raw.githubusercontent.com/stevemclaugh/audio-labeling-container/master/static/wavesurfer/wavesurfer.minimap.min.js
+wget -N https://raw.githubusercontent.com/stevemclaugh/audio-labeling-container/master/static/wavesurfer/wavesurfer.regions.js
+wget -N https://raw.githubusercontent.com/stevemclaugh/audio-labeling-container/master/static/wavesurfer/wavesurfer.regions.min.js
+wget -N https://raw.githubusercontent.com/stevemclaugh/audio-labeling-container/master/static/wavesurfer/wavesurfer.timeline.min.js
+
 cd /home/audio_labeler/
 python app.py
